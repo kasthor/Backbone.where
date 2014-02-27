65c03e69fe0f312905c523cfebfadfd498b3a3df
Giancarlo Palavicini <kasthor@gmail.com>
2014-02-27 08:38:30 -0600

    match conditions as class function

--- backbone.where.js
+++ backbone.where.js
@@ -2,17 +2,20 @@ _.extend( Backbone.Collection.prototype,{
   where: function( conditions ){
     var self = this;
     return self.filter( function( item ){
-      return self.match_conditions( item, conditions );
+      return Backbone.Collection.match_conditions( item, conditions );
     });
-  },
+  }
+})
+
+_.extend( Backbone.Collection, {
   match_conditions: function( item, conditions ){
     var self = this;
     for( var key in conditions ) {
       if( key == '$not' ) {
-        return ! self.match_conditions( item, conditions[key] );
+        return ! Backbone.Collection.match_conditions( item, conditions[key] );
       } else if ( key == '$or' ) {
         var _result = _( conditions[key] ).collect( function( _conditions ){ 
-          return self.match_conditions( item, _conditions );
+          return Backbone.Collection.match_conditions( item, _conditions );
         } )
         return _.any( _result );
       } else if( _.isArray( conditions[ key ] ) ){
